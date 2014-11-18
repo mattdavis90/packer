d9ea9193b65f8146c860d88a9dd77366d4a7e7af
Matt Davis <mattdavis90@googlemail.com>
2014-11-18 10:50:52 +0000

    Still not perfect packing

--- packer/packer2.py
+++ packer/packer2.py
@@ -16,9 +16,20 @@ class Packer(object):
 
         items = sorted(items, key=attrgetter('weight'), reverse=True)
         
+        # Search through the bins looking for space starting at the bin that was touched longest ago
         for item in items:
-            # Search through the bins looking for space starting at the bin that was touched longest ago
-            bins.append(bins.pop(0)) # Moves the touched bin to the end of the list
+            stored = False
+
+            for idx, location in enumerate(bins):
+                if location.weight < fill_limit and item.weight <= (fill_limit - location.weight):
+                    location.add_item(item)
+                    bins.append(bins.pop(idx)) # Moves the touched bin to the end of the list
+                    stored = True
+
+                    break
+
+            if not stored:
+                other.add_item(item)
         
         return (bins, other)
 
